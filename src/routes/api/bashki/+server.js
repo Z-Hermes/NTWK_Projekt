238498b8c9898d7d240b9f4e761187e46736bc6d
Z-Hermes <zallahermez@gmail.com>
2026-03-12 15:04:32 +0100

    Fifth commit with correct POST

--- src/routes/api/bashki/+server.js
+++ src/routes/api/bashki/+server.js
@@ -4,3 +4,14 @@ export async function GET() {
     const [rows] = await pool.query('SELECT * from bashki');
     return Response.json(rows);
 }
+
+export async function POST({request}) {
+
+    const { name, population, area, region, mayor, party, postal_code } = await request.json();
+
+    const [result] = await pool.query('INSERT INTO bashki (name, population, area, region, mayor, party, postal_code) VALUES (?,?,?,?,?,?,?)', [name, population, area, region, mayor, party, postal_code]);
+
+    return Response.json({ "message": "Bashki created"},{ status: 201})
+
+
+}
