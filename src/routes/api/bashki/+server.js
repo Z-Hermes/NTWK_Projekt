c3ccc5adc73e2e867e37d78d3a9f764145f7b9a9
Z-Hermes <zallahermez@gmail.com>
2026-03-12 15:53:06 +0100

    Basic Auth function

--- src/routes/api/bashki/+server.js
+++ src/routes/api/bashki/+server.js
@@ -5,13 +5,31 @@ export async function GET() {
     return Response.json(rows);
 }
 
-export async function POST({request}) {
+export async function POST({ request }) {
+    if (!checkAuth(request)) {
+        return Response.json({ error: 'Unauthorized' }, { status: 401 });
+    }
 
     const { name, population, area, region, mayor, party, postal_code } = await request.json();
+    const [result] = await pool.query(
+        'INSERT INTO bashki (name, population, area, region, mayor, party, postal_code ) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        [name, population, area, region, mayor, party, postal_code]
+    );
 
-    const [result] = await pool.query('INSERT INTO bashki (name, population, area, region, mayor, party, postal_code) VALUES (?,?,?,?,?,?,?)', [name, population, area, region, mayor, party, postal_code]);
+    return Response.json({ id: result.insertId, title, date }, { status: 201 });
+}
 
-    return Response.json({ "message": "Bashki created"},{ status: 201})
+import { API_USER, API_PASS } from '$env/static/private';
 
+function checkAuth(request) {
+    const auth = request.headers.get('Authorization');
 
+    if (!auth?.startsWith('Basic ')) return false;
+
+    const base64 = auth.slice(6);
+    const decoded = atob(base64);
+
+    const [user, pass] = decoded.split(':');
+
+    return user === API_USER && pass === API_PASS;
 }
