95b069abcd92b91c8274b47f3d65835b65b87710
Z-Hermes <zallahermez@gmail.com>
2026-03-13 08:17:59 +0100

    Final version, with Comments and finishing touches

--- src/routes/api/bashki/+server.js
+++ src/routes/api/bashki/+server.js
@@ -16,7 +16,7 @@ export async function POST({ request }) {
         [name, population, area, region, mayor, party, postal_code]
     );
 
-    return Response.json({ id: result.insertId, title, date }, { status: 201 });
+    return Response.json({ id: result.insertId, name, population, area, region, mayor, party, postal_code }, { status: 201 });
 }
 
 import { API_USER, API_PASS } from '$env/static/private';
@@ -24,11 +24,14 @@ import { API_USER, API_PASS } from '$env/static/private';
 function checkAuth(request) {
     const auth = request.headers.get('Authorization');
 
+    // if the auth doesnt start with "Basic " then it is invalid
     if (!auth?.startsWith('Basic ')) return false;
 
+    // we get rid of the "Basic ". The 6 stands for the 6 elements("Basic"5 elements and a space)
     const base64 = auth.slice(6);
     const decoded = atob(base64);
 
+     // we cut the Auth at the ':'
     const [user, pass] = decoded.split(':');
 
     return user === API_USER && pass === API_PASS;
